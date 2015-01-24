697e35ea33b7bc9a34cfdaddcad189108814e6a6
Eric Gauderman <eric.gauderman@gmail.com>
2015-01-24 15:48:47 -0800

    CloudCode temp test stuff

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -2,5 +2,9 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  var jsonResponse = {
+    hello: "SS12",
+    world: "Simon Says"
+  };
+  response.success(jsonResponse);
 });
