d7ba3df57401afe0eba4da14381d93600af253bb
Eric Gauderman <eric.gauderman@gmail.com>
2015-01-24 15:54:12 -0800

    CloudCode v4 (test)

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -2,9 +2,10 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  var jsonResponse = {
-    hello: "SS12",
-    world: "Simon Says"
-  };
-  response.success(jsonResponse);
+  // var jsonResponse = {
+  //   hello: "SS12",
+  //   world: "Simon Says"
+  // };
+  // response.success(jsonResponse);
+  response.success("SS12! ACM!");
 });
