b46e9f1f6177c6f9ba695af2aa7fa9203d317e15
Ryan Zhou <ryanzhou7@gmail.com>
2015-01-24 12:21:01 -0800

    pulling data

--- SS12_Simon_Says/app/src/main/java/com/example/douglass_macbook/ss12_simon_says/MainActivity.java
+++ SS12_Simon_Says/app/src/main/java/com/example/douglass_macbook/ss12_simon_says/MainActivity.java
@@ -2,17 +2,25 @@ package com.example.douglass_macbook.ss12_simon_says;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
     Button start_button;
     String userId;
+    ParseObject user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +32,9 @@ public class MainActivity extends ActionBarActivity {
         Parse.initialize(this, "9yqUakCzEIKfujACCHu063LqshOLUZrySAspjCO9", "rV1JpxIniI8YBl6dt5lG0bFj6r0TtE2uHUIn3Rx1");
 
         //"signing us user"
-        ParseObject user = new ParseObject("User");
+        user = new ParseObject("User");
         user.saveInBackground();
+        user.put("foo", "bar");
         userId = user.getObjectId();
 
         //assign this to match xml button
@@ -35,7 +44,19 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
                 //check if there are other users
                 //Parse
-
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+                query.whereEqualTo("foo", "bar");
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> parseObjects, com.parse.ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(getApplicationContext(), parseObjects.size(), Toast.LENGTH_SHORT);
+                            Log.d("score", "Retrieved " + parseObjects.size() + " scores");
+                        } else {
+                            Log.d("score", "Error: " + e.getMessage());
+                        }
+                    }
+                });
                 //Start the game
 
             }
