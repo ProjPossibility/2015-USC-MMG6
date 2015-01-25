ee13edab201ae4ca93c863e8970f2990211a79b5
Zach Zeff <zeff@usc.edu>
2015-01-25 00:38:16 -0800

    added check for time scheduling

--- SS12_Simon_Says/app/src/main/java/com/example/douglass_macbook/ss12_simon_says/ReadyActivity.java
+++ SS12_Simon_Says/app/src/main/java/com/example/douglass_macbook/ss12_simon_says/ReadyActivity.java
@@ -1,17 +1,71 @@
 package com.example.douglass_macbook.ss12_simon_says;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.parse.FunctionCallback;
+import com.parse.ParseCloud;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 
 public class ReadyActivity extends ActionBarActivity {
+
+    boolean simonSays;
+    ArrayList <Integer> arraylist;
+    int action;
+    long timeStamp;
+    Date now;
+    long millis;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ready);
+
+
+        ParseCloud.callFunctionInBackground("get_instruction",
+                new HashMap<String, Object>(),
+                new FunctionCallback<HashMap<String, Object>>() {
+                    @Override
+                    public void done(HashMap<String, Object> instruction, com.parse.ParseException e) {
+                        if (e == null) {
+                            simonSays = (boolean) instruction.get("simonSays");
+                            arraylist = (ArrayList<Integer>) instruction.get("who");
+                            action = (int) instruction.get("action");
+                            timeStamp = (long) instruction.get("timeStamp");
+                            //Date timeStampDate = (Date)instruction.get("timestamp");
+
+                            //SET PLAYER NUM ID
+                            //handleEvents();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Exception on server query", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+        now = new Date();
+        millis = now.getTime();
+
+        //get now time see if greater - if greater do immediately , else wait for time
+        if(millis>timeStamp){
+            //do immediately(go to gameActivity
+        } else {
+            //wait for time
+
+            now = new Date();
+            long millis2 = now.getTime();
+
+
+
+        }
+
     }
 
 
