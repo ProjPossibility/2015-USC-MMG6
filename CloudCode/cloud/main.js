b97a4fda662a77b1456375b8dfa527f86d3d0ef5
Douglass Chen <douglasschen@gmail.com>
2015-01-24 19:02:32 -0800

    updated main.js; attempting to add instruction

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,7 +1,7 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
+Parse.Cloud.define("get_instruction", function(request, response) {
   var jsonResponse = {
     simonSays: simonSays(),
     who: everyone(),
@@ -11,9 +11,47 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success(jsonResponse);
 });
 
+Parse.Cloud.define("join", function(request, response) {
+
+});
+
+Parse.Cloud.define("ready", function(request, response) {
+  var Instruction = Parse.Object.extend("Instruction");
+  var query = new Parse.Query(Instruction);
+
+  query.get("0", {
+    success: function(theInstruction) {
+
+    },
+    error: function(obj, error) {
+
+    }
+  });
+  var currInstruction = new Instruction();
+
+  currInstruction.set("simonSays", simonSays());
+  currInstruction.set("who", everyone());
+  currInstruction.set("action", actionNumber());
+  currInstruction.set("timeStamp", getTime());
+
+  // Note: hard-coded object ID
+  
+  currInstruction.set("objectId", "0");
+
+
+});
+
+Parse.Cloud.define("result", function(request, response) {
+
+});
+
+Parse.Cloud.define("leave", function(request, response) {
+
+});
+
 function simonSays() {
 	var generated = Math.random();
-	if( generated > 0.8){
+	if (generated > 0.8) {
 		//do a simon says
 		return true;
 	}
@@ -23,25 +61,16 @@ function simonSays() {
 }
 
 function everyone() {
-	var generated = Math.random();
-	if( generated > 0.7){
-		//do everyone
-		return true;
-	}
-	else{
-		//get users still in the game
-
-		return false;
-	}
+	return [1,2,3,4];
 }
 
 function actionNumber() {
-	var generated = Math.random()*16.0;
-	return generated;
+	// var generated = Math.random()*16.0;
+	// return generated;
+	return 1;
 }
 
 function getTime() { 
-	return new Date().getTime(); 
+	var date = new Date().getTime();
+	return date + 5000;
 }
-
-
