2840dbf9c752ebbcef4db6665c25bbb8b168b99b
Eric Gauderman <eric.gauderman@gmail.com>
2015-01-25 04:49:25 -0800

    put back enablelocaldatastore

--- SS12_Simon_Says/app/src/main/java/com/example/douglass_macbook/ss12_simon_says/App.java
+++ SS12_Simon_Says/app/src/main/java/com/example/douglass_macbook/ss12_simon_says/App.java
@@ -12,6 +12,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        Parse.enableLocalDatastore(this);
         Parse.initialize(this, "9yqUakCzEIKfujACCHu063LqshOLUZrySAspjCO9", "rV1JpxIniI8YBl6dt5lG0bFj6r0TtE2uHUIn3Rx1");
     }
 }
